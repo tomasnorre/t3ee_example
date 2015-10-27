7ff11651f81787bfdfc00c51bb232d9cee0e4615
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2015-10-27 22:00:39 +0100

    [TASK] Add TypoScript Linting

--- Configuration/TypoScript/constant.ts
+++ Configuration/TypoScript/constant.ts
@@ -3,16 +3,13 @@
 #	Configuration of EXT:t3ee_example
 #
 
-	plugin.tx_t3eeexample {
-		view {
-		# cat=plugin.tx_t3eeexample/file; type=string; label=Path to template root (FE)
-			templateRootPath = EXT:t3ee_example/Resources/Private/Templates/
-			 # cat=plugin.tx_t3eeexample/file; type=string; label=Path to template partials (FE)
-			partialRootPath = EXT:t3ee_example/Resources/Private/Partials/
-			 # cat=plugin.tx_t3eeexample/file; type=string; label=Path to template layouts (FE)
-			layoutRootPath = EXT:t3ee_example/Resources/Private/Layouts/
-		}
-		persistence {
-			storagePid = 0
-		}
-	}
+plugin.tx_t3eeexample {
+    view {
+        templateRootPath = EXT:t3ee_example/Resources/Private/Templates/
+        partialRootPath = EXT:t3ee_example/Resources/Private/Partials/
+        layoutRootPath = EXT:t3ee_example/Resources/Private/Layouts/
+    }
+    persistence {
+        storagePid = 0
+    }
+}
--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -3,14 +3,13 @@
 #	Configuration of EXT:t3ee_example
 #
 
-	plugin.tx_t3eeexample {
-		view {
-			templateRootPaths  = {$plugin.tx_t3eeexample.view.templateRootPath}
-			partialRootPaths   = {$plugin.tx_t3eeexample.view.partialRootPath}
-			layoutRootPaths    = {$plugin.tx_t3eeexample.view.layoutRootPath}
-		}
-		persistence {
-			storagePid = {$plugin.tx_t3eeexample.persistence.storagePid}
-		}
-	}
-
+plugin.tx_t3eeexample {
+    view {
+        templateRootPaths = {$plugin.tx_t3eeexample.view.templateRootPath}
+        partialRootPaths = {$plugin.tx_t3eeexample.view.partialRootPath}
+        layoutRootPaths = {$plugin.tx_t3eeexample.view.layoutRootPath}
+    }
+    persistence {
+        storagePid = {$plugin.tx_t3eeexample.persistence.storagePid}
+    }
+}
